afb58a1f44ebe0606f2e920418149002080caa25
Yukta Patil <162869052+YuktaP01@users.noreply.github.com>
2024-08-25 22:54:48 +0530

    Delete src directory

--- src/main/java/com/example/repository/TourRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Tour;
-
-public interface TourRepository extends JpaRepository<Tour, Integer> {
-}
